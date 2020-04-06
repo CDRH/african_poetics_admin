bbe3d61572b55b27481a095134922ad46db15c68
Jessica Dussault <jdussault4@gmail.com>
2020-04-06 11:09:09 -0500

    configure gender to not display created / updated info

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -2,4 +2,15 @@ class Gender < ApplicationRecord
 
   has_many :people
 
+  rails_admin do
+    list do
+      configure :created_at do
+        hide
+      end
+      configure :updated_at do
+        hide
+      end
+    end
+  end
+
 end

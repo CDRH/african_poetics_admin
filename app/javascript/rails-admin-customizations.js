bd5986e8c4ad5ba50ddf936f7bf818a70fb82522
Greg Tunink <techgique@gmail.com>
2020-10-16 18:07:31 -0500

    Open all filters on model index pages
    Close #31

--- app/javascript/rails-admin-customizations.js
+++ app/javascript/rails-admin-customizations.js
@@ -10,11 +10,17 @@ export function start() {
     if (page_header !== null) {
       var rails_admin_model = page_header.dataset.model;
 
-      if (rails_admin_model !== undefined &&
-          !MODELS_SHOWING_NEW_BUTTON.includes(rails_admin_model)) {
-        var new_item_link = document.querySelector(".nav-tabs .new_collection_link");
+      if (rails_admin_model !== undefined) {
+        if (!MODELS_SHOWING_NEW_BUTTON.includes(rails_admin_model)) {
+          var new_item_link = document.querySelector(".nav-tabs .new_collection_link");
 
-        new_item_link.style.display = "none";
+          new_item_link.style.display = "none";
+        }
+
+        // Open all filters on model index pages
+        var filter_toggles = document.querySelectorAll("#filters a");
+
+        filter_toggles.forEach(function (toggle) { toggle.click(); });
       }
     }
   });

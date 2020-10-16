66520697583d167cc0559531edd7a26c7b0a2ce0
Greg Tunink <techgique@gmail.com>
2020-10-16 17:30:51 -0500

    Highlight configurable array; minor code refactor
    Hope this makes this easier to configure in the future

--- app/javascript/rails-admin-customizations.js
+++ app/javascript/rails-admin-customizations.js
@@ -1,18 +1,20 @@
 export function start() {
   document.addEventListener("DOMContentLoaded", function() {
+    var MODELS_SHOWING_NEW_BUTTON = [
+      "news_item",
+      "event",
+    ];
+
     var page_header = document.querySelector(".page-header");
 
     if (page_header !== null) {
       var rails_admin_model = page_header.dataset.model;
 
-      if (rails_admin_model !== undefined) {
-        var models_showing_new_button = ["news_item", "event"];
+      if (rails_admin_model !== undefined &&
+          !MODELS_SHOWING_NEW_BUTTON.includes(rails_admin_model)) {
+        var new_item_link = document.querySelector(".nav-tabs .new_collection_link");
 
-        if (!models_showing_new_button.includes(rails_admin_model)) {
-          var new_item_link = document.querySelector(".nav-tabs .new_collection_link");
-
-          new_item_link.style.display = "none";
-        }
+        new_item_link.style.display = "none";
       }
     }
   });

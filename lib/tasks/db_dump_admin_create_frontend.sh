a6e48afb905bb13087496acd282f4fbd1f1a3d11
Jessica Dussault <jdussault4@gmail.com>
2021-01-06 09:21:06 -0600

    simplifies shell script slightly
    removes logging and a few variables not being used

--- lib/tasks/db_dump_admin_create_frontend.sh
+++ lib/tasks/db_dump_admin_create_frontend.sh
@@ -2,39 +2,21 @@
 
 set -eu
 
-# Notes
-# Variables left unquoted when list of tables of 'for' loop
-# and passed to mysqldump, so expanded & not treated as one long string
-
 # Options
 PROJECT_DIR="/var/local/www/rails/african_poetics-in_the_news"
-QUIET=0
 SUBDIR="db_dump"
 
 # get database configuration
 source "${PROJECT_DIR}/lib/tasks/config.sh"
 
 readonly BACKUP_DIR="${PROJECT_DIR}/${SUBDIR}"
-readonly QUIET
-readonly SUBDIR
-
 
 echo "Preparing backup directory"
 
 rm -rf "${BACKUP_DIR}"
-
-# Prep directory
 umask 007  # 770 dir permissions
 mkdir -p "${BACKUP_DIR}"
-
 umask 117  # 660 file permissions
-
-echo "Preparing log.txt"
-
-if [[ ${QUIET} -eq 1 ]]; then
-  # Redirect stderr to screen, stdout to log file
-  exec 2>&1 >"${BACKUP_DIR}/log.txt"
-fi
 
 echo "Preparing mysql tables"
 
@@ -53,4 +35,3 @@ mysqldump -u ${USER} -p${PSWD} --single-transaction "${DB_FROM}" ${innodb_tables
 # now that copy of admin database has been created, drop and create frontend database
 
 mysql -u ${USER} -p${PSWD} ${DB_TO} < "${BACKUP_DIR}/${DB_TO}.sql"
-
